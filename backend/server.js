4c75c60df4133029e5015beeef73146a73605d78
arpitjain1405 <arpitjain1405@gmail.com>
2025-06-25 21:59:37 +0530

    change cors origin

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(
   cors({
-    origin: true,
+    origin: ["https://compression-algorithms-project.vercel.app/", "http://localhost:5173"],
     credentials: true,
   }),
 )
